f10ad44b943b39624d3cc031720855c8533232ea
junlinpoo-hue <junlinpoo@gmail.com>
2026-06-07 18:11:06 +0800

    Update libwrt.sh

--- libwrt.sh
+++ libwrt.sh
@@ -6,3 +6,12 @@ rm -rf package/emortal/luci-app-athena-led
 rm -rf package/luci-app-athena-led
 
 
+# （可选）根治物理端口映射：交换 DTS 中 lan1 和 lan4 的 label
+
+DTSFILE="target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6000-zn-m2.dts"
+if [ -f "$DTSFILE" ]; then
+    echo "[Patch] Swapping lan1/lan4 label in DTS"
+    sed -i 's/label = "lan1"/label = "lan1_temp"/g' "$DTSFILE"
+    sed -i 's/label = "lan4"/label = "lan1"/g' "$DTSFILE"
+    sed -i 's/label = "lan1_temp"/label = "lan4"/g' "$DTSFILE"
+fi
